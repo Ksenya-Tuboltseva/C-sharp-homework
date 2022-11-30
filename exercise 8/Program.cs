4f4ccc839154253e4154f0ecf2df606a0a33772b
Ksenya <ksyuhacr7@gmail.com>
2022-11-30 23:17:32 +0300

    homework

---
+++ exercise 8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int index = 1; index <= N; index++)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine(index);
+    }
+};
